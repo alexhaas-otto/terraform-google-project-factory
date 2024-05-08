6c7648c40150139973d4e976115da96058529249
Alexander Haas <132475647+alexhaas-otto@users.noreply.github.com>
2024-05-08 10:16:54 +0200

    Update link to service agents documentation

--- modules/project_services/variables.tf
+++ modules/project_services/variables.tf
@@ -36,7 +36,7 @@ variable "activate_api_identities" {
     The list of service identities (Google Managed service account for the API) to force-create for the project (e.g. in order to grant additional roles).
     APIs in this list will automatically be appended to `activate_apis`.
     Not including the API in this list will follow the default behaviour for identity creation (which is usually when the first resource using the API is created).
-    Any roles (e.g. service agent role) must be explicitly listed. See https://cloud.google.com/iam/docs/understanding-roles#service-agent-roles-roles for a list of related roles.
+    Any roles (e.g. service agent role) must be explicitly listed. See https://cloud.google.com/iam/docs/service-agents for a list of related roles.
   EOF
   type = list(object({
     api   = string
